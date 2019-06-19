2aaf64b88bcd55c07f67f32726235f2a07a63192
kekland <kk.erzhan@gmail.com>
2019-06-19 19:51:08 +0600

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
