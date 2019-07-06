0dfb3c7753ba0f2ee4ce83254003d862773d53b1
kekland <kk.erzhan@gmail.com>
2019-07-06 19:47:31 +0600

    proposed change to EqDivider

--- lib/components/divider/divider.dart
+++ lib/components/divider/divider.dart
@@ -3,6 +3,12 @@ import 'package:equinox/equinox.dart';
 import 'package:flutter/widgets.dart';
 
 class EqDivider extends StatelessWidget {
+  final bool brightBackground;
+
+  const EqDivider({
+    Key key,
+    this.brightBackground = false,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     var theme = EqTheme.of(context);
